5342770bb0d9ee2d34ed247c424a54f59bac3f4a
DarkWizardZand <wizard102004@yandex.ru>
2023-10-07 10:57:23 +0500

    end

--- bin/kt2.dart
+++ bin/kt2.dart
@@ -1,5 +1,23 @@
+import 'package:dio/dio.dart';
 import 'package:kt2/kt2.dart' as kt2;
+import 'models/car/car.dart';
+import 'models/cars/cars.dart';
 
-void main(List<String> arguments) {
-  print('Hello world: ${kt2.calculate()}!');
+void main(List<String> arguments) async{
+  Dio client = Dio();
+  String url = "https://myfakeapi.com/api/cars";
+  Response res = await  client.get(url);
+
+  Carts data = Carts.fromJson(res.data);
+  int count = 0;
+  int medle = 0;
+  List<Cars> carsMas = data.cars;
+  for (var element in carsMas) {
+    if(element.car_color == 'Yellow'){
+      // print(1);
+      count++;
+      String medle = element.price.substring(1);
+    }
+  }
+  print(medle/count);
 }
